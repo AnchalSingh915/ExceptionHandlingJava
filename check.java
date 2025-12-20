c696d013e36d7d097bf58bba35260705b99c08e7
Anchal Singh <anchalsingh8736@gmail.com>
2025-12-20 18:03:13 +0530

    Add files via upload

---
+++ check.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class check
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);  
+        byte x=0;
+        System.out.println("enter the byte");
+        x=sc.nextByte();
+        System.out.println("output"+x);
+    }
+}
+       
+
+        
+        
